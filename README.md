ce0f42d0fba85212706bc3834cb531d5ecd2d5ef
Tristan <trra53@gmail.com>
2018-06-08 15:51:31 +0800

    A small update

--- README.md
+++ README.md
@@ -1,1 +1,89 @@
 # minify.js-api
+
+**A Very Small API based off from jQuery.**
+
+# How To Use
+
+### $$()
+ To call the function use '$$()' and between the parenthesis put the Element.
+ You can use the Element's id,class or Tagname
+
+**Example:**
+  ```javascript
+    //To call the function
+    $$()
+
+    //The function with the selected element
+    $$("#foo")
+    $$("div")
+    $$(".bar")
+  ```
+
+### List of Function in the API
+    *$$()
+        *write()
+        *html()
+        *value()
+        *color()
+        *bgColor()
+        *width()
+        *cssWidth()
+        *height()
+        *cssHeight()
+        *border()
+        *background()
+        *borderRadius()
+        *element (not a function)
+    *$e
+        *createEl()
+            *appendTo()
+
+
+
+## Function List of _$$()_
+
+Functions   |       Description       | Example
+------------------------------------------------
+write()     |Sets the innerText of the Selected Element | $$('#foo').write('hello world!')
+html()      |Sets the innerHTML of the Selected Element | $$('#foo').write('<span>hello world!</span>')
+value()     |Sets the value of the Selected Element can be a String or Number | $$('.bar').value(10)
+color()     |Sets the text color of the Selected Element | $$("div").color("yellow")
+bgColor()   |Sets the background color of the Selected Element | $$('.foo').bgColor('blue')
+width()     |Sets the width of the Element | $$('canvas').width(200)
+cssWidth()  |Sets the width of the Element in CSS | $$('fieldset').cssWidth("300px")
+height()    |Sets the height of the Element | $$('canvas').height(200)
+cssHeight() |Sets the height of the Element in CSS | $$('fieldset').cssHeight("400px")
+border()    |Sets the border of the Element. Takes 3 arguments | $$('div').border("2px", "solid", "black")
+background()|Sets the background of the Element. You can use the gradients in CSS | $$('#foo').background("url('foo.png')")
+borderRadius()|Sets the border-radius of the Element | $$('div').borderRadius(30px)
+Extra       |       Description       | Example
+-----------------------------------------------
+element     |This is not a function   | $("#foo").element.id
+
+## $e
+ **_$e_** is used to create Elements and append it to a selected element
+
+### Usage
+```javascript
+
+    //Just type $e
+    $e
+
+    //And call the createEl function
+    $e.createEl()
+
+    //Put the Element name between the parenthesis
+    $e.createEl("div")
+
+    //And call the appendTo function.
+    //The appendTo function appends the Created Element to the Selected Element
+    $e.createEl("div").appendTo()
+
+    //In the appendTo function put the Element id,class or tagname
+    $e.createEl("div").appendTo("#foo")
+
+    //The created Element have a default id of 'el' and you can edit it by
+    $e.createEl("div").appendTo("#foo")
+    $$("#el").element.id = "foo" //You can change the foo to any id you likes
+
+```
---
+++ main.js
@@ -0,0 +1,133 @@
+var $$ = a
+var $e = {
+    createEl: s
+}
+
+/**
+ * @param {Element} b 
+ * 
+ */
+function a(b) {
+    if (b == undefined || b == null || b == "") {
+        console.error("Error!: Undefined");
+    } else {
+        return c(b)
+    }
+}
+
+function c(d) {
+    var e = document.querySelector(d)
+    return {
+        /**
+         * Writes the inner text of the element (if possible)
+         * @param {String} f
+         */
+        write: function (f) {
+            e.innerText = f
+        },
+        /**
+         * Writes the inner HTML of the element (if possible)
+         * @param {String} g (e.g <div class="foo">hello world!</div>)
+         */
+        html: function (g) {
+            e.innerHTML = g
+        },
+        /**
+         * Sets the element value (if possible)
+         * @param {any} h String or Integer
+         */
+        value: function (h) {
+            e.value = h
+        },
+        /**
+         * Sets the text color of the element
+         * @param {String} i Color
+         */
+        color: function (i) {
+            e.style.color = i
+        },
+        /**
+         * Sets the background color of the element
+         * @param {String} j Background Color
+         */
+        bgColor: function (j) {
+            e.style.backgroundColor = j
+        },
+        /**
+         * Sets the Element width
+         * @param {Number} k Element width
+         */
+        width: function (k) {
+            e.width = k
+        },
+        /**
+         * Sets the Element width in CSS
+         * @param {String} l Element width in CSS 
+         * (e.g. $$("#foo").cssWidth("200px"))
+         */
+        cssWidth: function (l) {
+            e.style.width = l
+        },
+        /**
+         * Sets the Element height
+         * @param {Number} m Element height
+         */
+        height: function (m) {
+            e.height = m
+        },
+        /**
+         * Sets the Element height in CSS
+         * @param {String} o Element height in CSS
+         * (e.g. $$("#foo").cssHeight("300px"))
+         */
+        cssHeight: function (o) {
+            e.style.height = o
+        },
+        /**
+         * Sets the Border of the selected element
+         * @param {String} p Border Width
+         * @param {String} q Border Type
+         * @param {String} r Border Color
+         */
+        border: function (p, q, r) {
+            e.style.border = p + " " + q + " " + r
+        },
+        /**
+         * Sets the background od the element
+         * @param {String} y background
+         */
+        background: function (y) {
+            e.style.background = y
+        },
+        borderRadius: function (z) {
+            e.style.borderRadius = z
+        },
+        element: e
+    }
+}
+
+/**
+ * Creates an Element
+ * @param {Element} t HTML Element tag
+ * @description
+ * (e.g. $e("#foo").append("#bar")).
+ * (Note: The create Element have a default id of "el". 
+ * You can change it by "$$('#el').element.id = 'foo'".
+ * You can change the 'foo' to any id you like.)
+ */
+function s(t) {
+    var u = document.createElement(t)
+    u.id = "el"
+    return {
+        /**
+         * Append the Element to a Element
+         * @param {Element} v
+         */
+        appendTo: function (v) {
+            var w = document.querySelector(v)
+            w.appendChild(u)
+        }
+    }
+}
+
+console.log("The API is Loaded and ready to use")
